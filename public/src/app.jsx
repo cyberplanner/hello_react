fe6e2230a4413fd4f89f34a0da3825fe5c0c87a2
cyberplanner <sal@salweb.co.uk>
2017-04-22 19:11:20 +0100

    Set default props

--- public/src/app.jsx
+++ public/src/app.jsx
@@ -1,8 +1,14 @@
 var Greeter = React.createClass({
+  getDefaultProps: function() {
+    return {
+      name: 'React'
+    };
+  },
   render: function() {
+    var name = this.props.name;
     return (
       <div>
-        <h1>Hello React!</h1>
+        <h1>Hello {name}!</h1>
         <p>This is from a component!</p>
       </div>
     );
