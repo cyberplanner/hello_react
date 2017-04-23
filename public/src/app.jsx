02d8b8456bb38efacd4b7f7733899380af7a1384
cyberplanner <sal@salweb.co.uk>
2017-04-23 14:35:05 +0100

    14. Component State: Changing state

--- public/src/app.jsx
+++ public/src/app.jsx
@@ -5,13 +5,36 @@ var Greeter = React.createClass({
       msg: 'This is from a component!'
     };
   },
+  getInitialState() {
+    return {
+      name: this.props.name
+    };
+  },
+  onButtonClick: function(e) {
+    e.preventDefault();
+
+    var nameRef = this.refs.name;
+    var name = nameRef.value;
+    nameRef.value = '';
+
+    if (typeof name === 'string' && name.length > 0 ) {
+      this.setState({
+        name: name
+      });
+    }
+  },
   render: function() {
-    var name = this.props.name;
+    var name = this.state.name;
     var msg = this.props.msg;
     return (
       <div>
         <h1>Hello {name}!</h1>
         <p>{msg}</p>
+
+        <form onSubmit={this.onButtonClick}>
+          <input type="text" ref="name"></input>
+          <button>Set Name</button>
+        </form>
       </div>
     );
   }
