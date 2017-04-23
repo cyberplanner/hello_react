e62f16ffbe6acf35d2844224de093bc75461be07
cyberplanner <sal@salweb.co.uk>
2017-04-23 03:46:30 +0100

    :construction: WIP

--- public/src/app.jsx
+++ public/src/app.jsx
@@ -1,22 +1,25 @@
 var Greeter = React.createClass({
   getDefaultProps: function() {
     return {
-      name: 'React'
+      name: 'React',
+      msg: 'This is from a component!'
     };
   },
   render: function() {
     var name = this.props.name;
+    var msg = this.props.msg;
     return (
       <div>
         <h1>Hello {name}!</h1>
-        <p>This is from a component!</p>
+        <p>{msg}</p>
       </div>
     );
   }
 });
 
-
+var firstName = 'Sal';
+var myMsg;
 ReactDOM.render(
-        <Greeter/>,
+        <Greeter name={firstName} msg={myMsg}/>,
         document.getElementById('app')
       );
