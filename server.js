025b6d68b061ea13ece218246344f4032197c738
cyberplanner <sal@salweb.co.uk>
2017-04-24 15:01:49 +0100

    Change port in server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app     = express();          // create an app
 app.use(express.static('public'));// Share a directory
 
 // listen to port 3000
-app.listen(3000, function() {
+app.listen(4000, function() {
   // and log a msg to the console
-  console.log('express server is up on port 3000');
+  console.log('express server is up on port 4000');
 });
